b554b775f89eafc017f65d7c85a997d74324dce1
momodou khan <khanmomodou101@mail.com>
2024-11-19 22:24:04 +0000

    update

---
+++ dinks/v1/auth.py
@@ -0,0 +1,109 @@
+import frappe
+from frappe import auth
+
+@frappe.whitelist(allow_guest=True)
+def login(usr, pwd):
+    try:
+        # Initialize and authenticate the login manager
+        login_manager = frappe.auth.LoginManager()
+        login_manager.authenticate(user=usr, pwd=pwd)
+        login_manager.post_login()
+    except frappe.exceptions.AuthenticationError:
+        frappe.clear_messages()
+        frappe.local.response["message"] = {
+            "success_key": 0,
+            "message": "Invalid login credentials"
+        }
+        return
+    # Generate API key and secret
+
+    api_secret = generate_keys(frappe.session.user)
+    user = frappe.get_doc('User', frappe.session.user)
+    data = {
+
+    
+        "success_key": 1,
+        "message": "Authentication success",
+        "sid": frappe.session.sid,
+        "api_key": user.api_key,
+        "api_secret": api_secret,
+        "username": user.username,
+        "email": user.email
+        
+    }
+    
+    frappe.response["message"] = data
+def generate_keys(user):
+    user_details = frappe.get_doc('User', user)
+    
+    # Generate a new API secret
+    api_secret = frappe.generate_hash(length=15)
+
+    # If API key doesn't exist, generate it
+    if not user_details.api_key:
+        api_key = frappe.generate_hash(length=15)
+        user_details.api_key = api_key
+
+    # Set the API secret
+    user_details.api_secret = api_secret
+    
+    # Save the user details to update the document
+    user_details.save(ignore_permissions=True)
+    frappe.db.commit()
+
+    return api_secret
+
+
+
+@frappe.whitelist(allow_guest=True)
+def signup():
+    try:
+        data = frappe.local.form_dict
+        first_name = data.get("first_name")
+        last_name = data.get("last_name")
+        email = data.get("email")
+        password = data.get("password")
+        gender = data.get("gender")
+        date_of_birth = data.get("date_of_birth")
+        mobile_number = data.get("mobile_number")
+        
+
+
+        if frappe.db.exists("User", email):
+            return {
+                "success_key": 0,
+                "error": "User email already registered, please login"
+            }
+
+        elif frappe.db.exists("User", {"mobile_no": mobile_number}):
+            return {
+                "success_key": 0,
+                "error": "Mobile number already registered, please login"
+            }
+
+        else:
+            user = frappe.new_doc("User")
+            user.first_name = first_name
+            user.last_name = last_name
+            user.email = email
+            user.new_password = password
+            user.send_welcome_email = 0
+            user.gender = gender
+            user.birth_date = date_of_birth
+
+            return {
+                "message": "Your account has been created successfully",
+                "success_key": 1
+            }
+
+    except Exception as e:
+        # Rollback in case of error to avoid partial creation
+        frappe.log_error(frappe.get_traceback(), ("Failed to create user"))
+        return {
+            "success_key": 0,
+            "error": str(e)
+        }
+
+        # return user.name
+
+
---
+++ dinks/v1/user.py
@@ -0,0 +1,45 @@
+import frappe
+@frappe.whitelist(allow_guest=True)
+def reset_password(email):
+    try:
+        user = frappe.get_doc('User', email)
+        return {
+            "success_key": 1,
+            "message": user.reset_password(send_email=True, password_expired=True)
+        }
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), ("Failed to reset password"))
+        return {
+            "success_key": 0,
+            "error": str(e)
+        }
+@frappe.whitelist(allow_guest=True)
+def change_password(email, old_password, new_password):
+    pass
+@frappe.whitelist( allow_guest=True )
+def profile():
+    try:
+        data = frappe.local.form_dict
+        first_name = data.get("first_name")
+        last_name = data.get("last_name")
+        email = data.get("email")
+        mobile_number = data.get("mobile_number")
+        date_of_birth = data.get("birth_date")
+        geder = data.get("gender")
+
+        user = frappe.get_doc('User', frappe.session.user)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email = email
+        user.mobile_no = mobile_number
+        user.birth_date = date_of_birth
+        uuser.save()
+
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), ("Failed to update profile"))
+        return {
+            "success_key": 0,
+            "error": str(e)
+        }
+
+
